31aea0513473124f0a23445576b31977af5ae462
Tan25x <sulthanradhitya07@gmail.com>
2026-02-08 18:22:16 +0700

    feat: Rename application and add core dependencies for authentication, database, and UI components.

--- src/lib/api/index.ts
+++
@@ -1,4 +0,0 @@
-// API barrel export
-export * from "./shivrahub";
-export * from "./anime";
-export * from "./manga";
--- src/lib/storage/index.ts
+++
@@ -1,3 +0,0 @@
-// Storage barrel export
-export * from "./favorites";
-export * from "./history";
